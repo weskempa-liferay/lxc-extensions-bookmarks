616a9fce92c73ed2dff00d92d2377b80d1a524d2
wesley.kempa <wesley.kempa@liferay.com>
2022-08-08 17:52:26 -0400

    Update index.js

--- nodejs/app/react/src/index.js
+++ nodejs/app/react/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './App.css';
 
 class LXCBookmarkApp extends HTMLElement {
   connectedCallback() {
